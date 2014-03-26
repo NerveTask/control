e382de97bf6ba192cdb0c12b31145308956d67c9
Patrick Daly <patrick@developdaly.com>
2014-03-25 21:56:46 -0500

    Hide login-required alerts

--- functions.php
+++ functions.php
@@ -78,7 +78,7 @@ add_action( 'widgets_init', 'control_widgets_init' );
 function control_scripts() {
 
 	wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Roboto:400,300,700,500' );
-	wp_enqueue_style('control_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '83812a7ac9b9925960f27601fd25c60a');
+	wp_enqueue_style('control_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7235aad4789ec6024fd59639fb248743');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
