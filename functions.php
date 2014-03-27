2fde13389e770abe48252de90f6d6d79407ff705
Patrick Daly <patrick@developdaly.com>
2014-03-26 21:50:10 -0500

    Sorted table colors

--- functions.php
+++ functions.php
@@ -78,7 +78,7 @@ add_action( 'widgets_init', 'control_widgets_init' );
 function control_scripts() {
 
 	wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Roboto:400,300,700,500' );
-	wp_enqueue_style('control_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '79b85eeb0b7fd0523f5ed79a9956c501');
+	wp_enqueue_style('control_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f32866911f56673f396d98a967f4bb1e');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
